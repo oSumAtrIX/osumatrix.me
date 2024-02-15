61ec7b4c26a4800e311b95132a94bb4f7166b496
oSumAtrIX <johan.melkonyan1@web.de>
2024-02-15 22:57:42 +0100

    chore: Update about me

--- src/components/molecules/About.svelte
+++ src/components/molecules/About.svelte
@@ -6,13 +6,13 @@
 <section>
 	<SectionHeader text="About me" icon={faAddressCard} />
 	<p>
-		21-year-old hyper-enthusiastic full-time blockhead, who had the great idea to study computer
-		science at the <a target="_blank" href="https://uni-augsburg.de/">University of Augsburg</a>.
+		22-year-old computer science student 
+		at <a target="_blank" href="https://uni-augsburg.de/">University of Augsburg</a>.
 		<br />
 		My interests and expertise expand from graphics-, motion- and sound design in dedicated software
 		like Adobe Suite and Image-Line FL Studio up to programming in lots of different languages like
-		<b>C#</b>,
-		<b>Rust</b>, <b>Kotlin</b>, <b>Java</b>, <b>PHP</b>, <b>HTML</b>, <b>JavaScript</b>, <b>CSS</b>,
+		<b>Kotlin</b>,
+		<b>Rust</b>, <b>C#</b>, <b>Java</b>, <b>PHP</b>, <b>HTML</b>, <b>JavaScript</b>, <b>CSS</b>,
 		<b>MySQL</b>, <b>Objective-C</b>, <b>C</b>, <b>Python</b> and <b>Visual Basic</b>, with the
 		first being my most and last being my least used languages. I mostly use my brief time
 		productively, but if I don't, then I usually play <a href="#chess">chess</a>,
